790bbee1d74f765079a737425b851c4ffdb1e01d
SuperMaxy <161463559+wyzbdwbhzlp@users.noreply.github.com>
2025-08-30 11:49:08 +0800

    破碎墙整合

---
+++ LeviSimulator/Assets/Scripts/Obstacles/BrokenWall.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BrokenWall : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Animator anim;
+    [SerializeField] Rigidbody rig;
+    void Start()
+    {
+        anim=GetComponent<Animator>();
+        rig = GetComponentInChildren<Rigidbody>();
+        rig.useGravity = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("Crushed", true);
+            rig.useGravity = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
