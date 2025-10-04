075aded67c200badd4a979494b772e7fd9aec35b
MingTea <1826352748@qq.com>
2025-10-04 18:00:15 +0800

    refactor: 重构光标锁定逻辑，统一使用PlayerController管理光标状态

--- LeviSimulator/Assets/Scripts/CollectibleEcho/CollectibleEchoController.cs
+++ LeviSimulator/Assets/Scripts/CollectibleEcho/CollectibleEchoController.cs
@@ -1,5 +1,6 @@
 ﻿using GlobalGameManager;
 using Manager;
+using PlayerControllers.Refactored;
 using PlayerControllers.Refactored.Systems;
 using Sirenix.OdinInspector;
 using UIManager;
@@ -43,14 +44,14 @@ namespace CollectibleEcho
                 echoDisplayViewUI.CloseButton.onClick.AddListener(CloseEcho);
                 
                 echoDisplayViewUI.ShowUIPanel(echoData);
-                PlayerCameraSystem.UnlockAndShowCursor(); //锁定并隐藏鼠标
+                PlayerController.UnlockAndShowCursor(); //锁定并隐藏鼠标
                 EventBroadcaster.CallEchoViewUIOpened(echoId);
             }
         }
         [Button("关闭回声UI")]
         public void CloseEcho()
         {
-            PlayerCameraSystem.LockAndHideCursor(); //解锁并显示鼠标
+            PlayerController.LockAndHideCursor(); //解锁并显示鼠标
             MainUIManager.HideUIComponent<EchoDisplayViewUI>();
             EventBroadcaster.CallEchoViewUIClosed();
         }
